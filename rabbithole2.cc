3dfde35abf35b64a96f6631369af69c2eace3be5
frvnk <frvnkliu@gmail.com>
2025-05-05 15:56:36 -0400

    22/22 Test Cases passed, modified return value of helper function

--- rabbithole2.cc
+++ rabbithole2.cc
@@ -10,50 +10,57 @@ struct vProp {
 };
 
 /*Modified Tarjans
-  Returns number of visitable pages for a given vertex v
+  Returns number of visitable pages for a given vertex v if it is the root of its scc
+  Otherwise return the max visitable pages of it's children not in the scc
 */
 int getVisitableWebpages(int v, int& index, vector<vProp>& vProps, stack<int>& S,  vector<unordered_set<int>>& adj){
-  //Lots of pass by reference variables, idk if this is good practice
-  vProps[v].index = vProps[v].lowlink = index++;
-  int currMax = 0; //max visitable of descendents
+  vProps[v].index = vProps[v].lowlink = index++; 
+  
+  //max visitable of descendents of this SCC
+  int maxDesc = 0; 
   int start = S.size();
   S.push(v);
   for(int w : adj[v]){
-    if(!vProps[w].index){
-      currMax = max(currMax, getVisitableWebpages(w, index, vProps, S, adj));
+    if(!vProps[w].index){ 
+      //Completely Unexplored
+      maxDesc = max(maxDesc, getVisitableWebpages(w, index, vProps, S, adj));
       vProps[v].lowlink = min(vProps[v].lowlink, vProps[w].lowlink);
-    }else if(!vProps[w].visitable){ //equivalent to being on stack since vProps[w].index != 0
-      vProps[v].lowlink = min(vProps[v].lowlink, vProps[w].index);
+    } else if(!vProps[w].visitable){ 
+      //Equivalent to being on stack since vProps[w].index != 0
+      vProps[v].lowlink = min(vProps[v].lowlink, vProps[w].lowlink);
     }else{
-      currMax = max(currMax, vProps[w].visitable);
+      maxDesc = max(maxDesc, vProps[w].visitable);
     }
   }
+
+  //Update all nodes in SCC rooted at v
   if(vProps[v].lowlink == vProps[v].index){
-    //Update all nodes in current SCC
-    int currVisitable = currMax + S.size()-start;//visitable from current cycle
+    int currVisitable = maxDesc + S.size()-start; //visitable from current cycle
     while(S.top() != v){
       vProps[S.top()].visitable =  currVisitable;
       S.pop();
     }
     S.pop();
     vProps[v].visitable = currVisitable;
+    return vProps[v].visitable;
   }
-  return vProps[v].visitable;
+  
+  //return Max Visitable number of descendents of this node not in the SCC
+  return maxDesc; 
 }
 
 int getMaxVisitableWebpages(int N, int M, vector<int> A, vector<int> B) {
-  // 1) Build original graph, dropping duplicate links
-
+  //1) Build Adjacent graph, dropping duplicate links
   vector<unordered_set<int>> adj(N);
-
   //Shift page numbering by 1 for better indexing
   for (int i = 0; i < M; i++) adj[A[i]-1].insert(B[i]-1);
 
-  // 2) Modified Tarjan's
+
+  //2) Modified Tarjans Algorithm
   int res = 0;
   int index = 1;
-  vector<vProp> vProps(N); //vertex properties
-  stack<int> S; //Stack
+  vector<vProp> vProps(N);
+  stack<int> S;
   for(int v = 0; v < N; v++){
     if(!vProps[v].index){
       res = max(res, getVisitableWebpages(v, index, vProps, S, adj));
